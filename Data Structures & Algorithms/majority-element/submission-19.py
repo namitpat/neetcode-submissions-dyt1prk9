619b0fe1a28b22e6437d6bf41124bcfc6d484ff5
namitpat <123006422+namitpat@users.noreply.github.com>
2026-03-30 19:50:59 +0530

    Add: majority-element - submission-19

---
+++ Data Structures & Algorithms/majority-element/submission-19.py
@@ -0,0 +1,48 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        visited=set()
+        target=0
+        k=1
+        n=len(nums)
+        n=n/2
+        for i in range(len(nums)):
+            k=1
+            if nums[i] in visited:
+                continue
+            visited.add(nums[i])
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    k=k+1
+            if k>n:
+                target=nums[i]
+        return target
+               
+            
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         target=0
+#         k=1
+#         n=len(nums)
+#         n=n/2
+#         for i in range(len(nums)):
+#             k=1
+#             if target !=nums[i]:
+#                 for j in range(i+1,len(nums)):
+#                     if nums[i]==nums[j]:
+#                         k=k+1
+#                 if k>n:
+#                     target=nums[i]
+
+            
+#         return target         
+
+
+            
+            
+            
+
+
+                    
+
+        
+        
